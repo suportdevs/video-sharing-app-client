ebcaa2e9258c5704cfdd08d0fd114a9778215a48
suportdevs <suportdevs@gmail.com>
2024-01-06 17:32:35 +0600

    description

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -19,6 +19,7 @@ const VideoContainer = styled.div`
 const VideoTitle = styled.h1`
     font-size: 26px;
     font-weight: 500;
+    margin: 10px 0;
     color: ${({theme}) => theme.text};
 `;
 const ChannelContainer = styled.div`
@@ -36,16 +37,17 @@ const ChannelImg = styled.img`
     height: 40px;
     border-radius: 50%;
     background-color: #999;
+    cursor: pointer;
 `;
 const ChannelTexts = styled.div``;
-const ChannelName = styled.h2`
+const ChannelName = styled.h1`
     font-size: 20px;
     font-weight: 500;
     color: ${({theme})=> theme.text};
+    cursor: pointer;
 `;
 const ChannelSubscribers = styled.p`
     font-size: 14px;
-    font-weight: 500;
     color: ${({theme})=> theme.textSoft};
 `;
 const Subscribe = styled.button`
@@ -54,6 +56,7 @@ const Subscribe = styled.button`
     border-radius: 50px;
     background-color: #eee;
     margin-left: 15px;
+    cursor: pointer;
 `;
 const Buttons = styled.div`
     display: flex;
@@ -62,12 +65,27 @@ const Buttons = styled.div`
 `;
 const Button = styled.button`
     display: flex;
+    gap: 5px;
     align-items: center;
     border-radius: 5px;
     border: none;
     background-color: transparent;
     cursor: pointer;
+    color: ${({ theme }) => theme.text};
 `;
+const Hr = styled.hr`
+    border: 0.5px solid ${({theme}) => theme.soft};
+    margin: 10px 0;
+`;
+const Description = styled.div`
+    padding: 10px 0;
+    font-size: 14px;
+    color: ${({theme}) => theme.text};
+`
+const DescriptionTopText = styled.div`
+    margin-bottom: 8px;
+`;
+const DescriptionText = styled.p``;
 const Recomended = styled.div`
     flex: 2;
 `;
@@ -106,7 +124,15 @@ const Video = () => {
                         <Button><PlaylistAddOutlinedIcon /> Save</Button>
                     </Buttons>
                 </ChannelContainer>
+                <Hr />
+                <Description>
+                    <DescriptionTopText>
+                        89,907 views  Jun 30, 2022  React.js Real-World Projects
+                    </DescriptionTopText>
+                    <DescriptionText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore voluptatem quas laudantium harum, iure nulla, iusto dolorum, commodi modi maxime debitis cum architecto. Aliquid magni sit nobis enim atque tempore dolorem deleniti saepe esse, omnis autem, qui aspernatur inventore fuga reiciendis facilis. Facilis expedita consequuntur minus amet aliquam dolorum vel dignissimos. Similique ipsam reprehenderit, quo vero, pariatur hic voluptas vel quidem nulla minima cumque voluptates natus facere mollitia minus! Quia atque officia sit animi, blanditiis incidunt rem reprehenderit similique quaerat nihil exercitationem mollitia aspernatur voluptatum, impedit itaque, tempore ratione. Laborum et minima voluptatibus. Perspiciatis ipsum dolores adipisci sapiente inventore quae!</DescriptionText>
+                </Description>
             </Content>
+
             <Recomended>Recomended</Recomended>
         </Container>
     );
